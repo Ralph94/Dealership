13d45bd8096d998e8a741ced48191840d2b0b867
Rafa24 <Raffp737@gmail.com>
2020-10-19 21:21:31 -0700

    Update - Dealership

---
+++ core_frontend/src/components/information.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Information() {
+  return (
+    <div className="information">
+    <h1>Information page!</h1>
+    </div>
+
+   );
+ }
+
+export default Information;
