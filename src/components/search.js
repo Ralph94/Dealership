5d356c90b20524684c205c3078ed24f62b8f795b
Rafa24 <46285634+Ralph94@users.noreply.github.com>
2020-10-19 21:28:52 -0700

    Add files via upload

---
+++ src/components/search.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+function Search() {
+  return (
+      <div className="search">
+      <h1>Search page!</h1>
+      <input type="text"
+      placeholder="Search.."/>
+      </div>
+
+
+
+
+  );
+}
+
+export default Search;
