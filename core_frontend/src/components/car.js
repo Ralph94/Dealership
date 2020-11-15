5ee43a4b1960abd49bb2c42ec0817af5a5c5c946
Rafa24 <Raffp737@gmail.com>
2020-11-14 23:20:41 -0800

    update

--- core_frontend/src/components/car.js
+++ core_frontend/src/components/car.js
@@ -35,6 +35,78 @@ import FordI3 from './fordI3.jpg';
 import TacomaI from './ti.jpg';
 import TacomaI2 from './ti2.jpg';
 import TacomaI3 from './ti3.jpg';
+import DodgeI from './DodgeI.jpg';
+import DodgeI2 from './DodgeI2.jpg';
+import DodgeI3 from './DodgeI3.jpg'
+import Dodge2I from './Dodge2I.jpg';
+import Dodge2I2 from './Dodge2I2.jpg';
+import Dodge2I3 from './Dodge2I3.jpg'
+import RamI from './ramI.jpg';
+import RamI2 from './ramI2.jpg';
+import RamI3 from './ramI3.jpg';
+import JeepcI from './jeepcI.jpg';
+import JeepcI2 from './jeepcI2.jpg';
+import JeepcI3 from './jeepcI3.jpg';
+import JeeprI from './jeeprI.jpg';
+import JeeprI2 from './jeeprI2.jpg';
+import JeeprI3 from './jeeprI3.jpg';
+import Jeepl1 from './jeepl1.jpg';
+import Jeepl2 from './jeepl2.jpg';
+import Jeepl3 from './jeepl3.jpg';
+import JeepwI from './jeepwI.jpg';
+import JeepwI2 from './jeepwI2.jpg';
+import JeepwI3 from './jeepwI3.jpg';
+import JeepsI from './jeepsI.jpg';
+import JeepsI2 from './jeepsI2.jpg';
+import JeepsI3 from './jeepsI3.jpg';
+import TCI from './TCI.jpg';
+import TCI2 from './TCI2.jpg';
+import TCI3 from './TCI3.jpg';
+import TC2I from './TC2I.jpg';
+import TC2I2 from './TC2I2.jpg';
+import TC2I3 from './TC2I3.jpg';
+import TRDI from './TRDI.jpg';
+import TRDI2 from './TRDI2.jpg';
+import TRDI3 from './TRDI3.jpg';
+import FI from './FI.jpg';
+import FI2 from './FI2.jpg';
+import FI3 from './FI3.jpg';
+import SEI from './SEI.jpg';
+import SEI2 from './SEI2.jpg';
+import SEI3 from './SEI3.jpg';
+import RVI from './RVI.jpg';
+import RVI2 from './RVI2.jpg';
+import RVI3 from './RVI3.jpg';
+import RV2I from './RV2I.jpg';
+import RV2I2 from './RV2I2.jpg';
+import RV2I3 from './RV2I3.jpg';
+import SOI from './SOI.jpg';
+import SOI2 from './SOI2.jpg';
+import SOI3 from './SOI3.jpg';
+import SFI from './SFI.jpg';
+import SFI2 from './SFI2.jpg';
+import SFI3 from './SFI3.jpg';
+import SF2I from './SF2I.jpg';
+import SF2I2 from './SF2I2.jpg';
+import SF2I3 from './SF2I3.jpg';
+import LMI from './LMI.jpg';
+import LMI2 from './LMI2.jpg';
+import LMI3 from './LMI3.jpg';
+import TVI from './TVI.jpg';
+import TVI2 from './TVI2.jpg';
+import TVI3 from './TVI3.jpg';
+import FMI from './FMI.jpg';
+import FMI2 from './FMI2.jpg';
+import FMI3 from './FMI3.jpg';
+import VEI from './VEI.jpg';
+import VEI2 from './VEI2.jpg';
+import VEI3 from './VEI3.jpg';
+import CEI from './CEI.jpg';
+import CEI2 from './CEI2.jpg';
+import CEI3 from './CEI3.jpg';
+
+
+
 import ImageGallery from 'react-image-gallery';
 
 const images = [
@@ -56,6 +128,436 @@ const images = [
  ];
 
 
+ const imageF = [
+  {
+    original: FordI,
+    thumbnail: FordI,
+  },
+
+  {
+    original: FordI2,
+    thumbnail: FordI2,
+  },
+
+   {
+    original: FordI3,
+    thumbnail: FordI3,
+  },
+
+ ];
+
+
+ const imageD = [
+  {
+    original: DodgeI,
+    thumbnail: DodgeI,
+  },
+
+  {
+    original: DodgeI2,
+    thumbnail: DodgeI2,
+  },
+
+   {
+    original: DodgeI3,
+    thumbnail: DodgeI3,
+  },
+
+ ];
+
+
+
+ const imageD2 = [
+  {
+    original: Dodge2I,
+    thumbnail: Dodge2I,
+  },
+
+  {
+    original: Dodge2I2,
+    thumbnail: Dodge2I2,
+  },
+
+   {
+    original: Dodge2I3,
+    thumbnail: Dodge2I3,
+  },
+
+ ];
+
+
+ const imageR = [
+  {
+    original: RamI,
+    thumbnail: RamI,
+  },
+
+  {
+    original: RamI2,
+    thumbnail: RamI2,
+  },
+
+   {
+    original: RamI3,
+    thumbnail: RamI3,
+  },
+
+ ];
+
+
+   const imageJC = [
+  {
+    original: JeepcI,
+    thumbnail: JeepcI,
+  },
+
+  {
+    original: JeepcI2,
+    thumbnail: JeepcI2,
+  },
+
+   {
+    original: JeepcI3,
+    thumbnail: JeepcI3,
+  },
+
+ ];
+
+
+
+const imageJR = [
+  {
+    original: JeeprI,
+    thumbnail: JeeprI,
+  },
+
+  {
+    original: JeeprI2,
+    thumbnail: JeeprI2,
+  },
+
+   {
+    original: JeeprI3,
+    thumbnail: JeeprI3,
+  },
+
+ ];
+
+
+ const imageJW = [
+  {
+    original: Jeepl1,
+    thumbnail: Jeepl1,
+  },
+
+  {
+    original: Jeepl2,
+    thumbnail: Jeepl2,
+  },
+
+   {
+    original: Jeepl3,
+    thumbnail: Jeepl3,
+  },
+
+ ];
+
+
+  const imageJL = [
+  {
+    original: JeepwI,
+    thumbnail: JeepwI,
+  },
+
+  {
+    original: JeepwI2,
+    thumbnail: JeepwI2,
+  },
+
+   {
+    original: JeepwI3,
+    thumbnail: JeepwI3,
+  },
+
+ ];
+
+
+   const imageJS = [
+  {
+    original: JeepsI,
+    thumbnail: JeepsI,
+  },
+
+  {
+    original: JeepsI2,
+    thumbnail: JeepsI2,
+  },
+
+   {
+    original: JeepsI3,
+    thumbnail: JeepsI3,
+  },
+
+ ];
+
+
+ const imageTC = [
+  {
+    original: TCI,
+    thumbnail: TCI,
+  },
+
+   {
+    original: TCI2,
+    thumbnail: TCI2,
+  },
+
+   {
+    original: TCI3,
+    thumbnail: TCI3,
+  },
+
+ ];
+
+
+  const imageTC2 = [
+  {
+    original: TC2I,
+    thumbnail: TC2I,
+  },
+
+   {
+    original: TC2I2,
+    thumbnail: TC2I2,
+  },
+
+   {
+    original: TC2I3,
+    thumbnail: TC2I3,
+  },
+ ];
+
+
+ const imageTRD = [
+  {
+    original: TRDI,
+    thumbnail: TRDI,
+  },
+
+   {
+    original: TRDI2,
+    thumbnail: TRDI2,
+  },
+
+   {
+    original: TRDI3,
+    thumbnail: TRDI3,
+  },
+ ];
+
+  const imageFI = [
+  {
+    original: FI,
+    thumbnail: FI,
+  },
+
+   {
+    original: FI2,
+    thumbnail: FI2,
+  },
+
+   {
+    original: FI3,
+    thumbnail: FI3,
+  },
+ ];
+
+  const imageSE = [
+  {
+    original: SEI,
+    thumbnail: SEI,
+  },
+
+   {
+    original: SEI2,
+    thumbnail: SEI2,
+  },
+
+   {
+    original: SEI3,
+    thumbnail: SEI3,
+  },
+ ];
+
+  const imageRV = [
+  {
+    original: RVI,
+    thumbnail: RVI,
+  },
+
+   {
+    original: RVI2,
+    thumbnail: RVI2,
+  },
+
+   {
+    original: RVI3,
+    thumbnail: RVI3,
+  },
+ ];
+
+ const imageRV2 = [
+  {
+    original: RV2I,
+    thumbnail: RV2I,
+  },
+
+   {
+    original: RV2I2,
+    thumbnail: RV2I2,
+  },
+
+   {
+    original: RV2I3,
+    thumbnail: RV2I3,
+  },
+ ];
+
+ const imageSO = [
+  {
+    original: SOI,
+    thumbnail: SOI,
+  },
+
+   {
+    original: SOI2,
+    thumbnail: SOI2,
+  },
+
+   {
+    original: SOI3,
+    thumbnail: SOI3,
+  },
+ ];
+
+ const imageSF = [
+  {
+    original: SFI,
+    thumbnail: SFI,
+  },
+
+   {
+    original: SFI2,
+    thumbnail: SFI2,
+  },
+
+   {
+    original: SFI3,
+    thumbnail: SFI3,
+  },
+ ];
+
+ const imageSF2 = [
+  {
+    original: SF2I,
+    thumbnail: SF2I,
+  },
+
+   {
+    original: SF2I2,
+    thumbnail: SF2I2,
+  },
+
+   {
+    original: SF2I3,
+    thumbnail: SF2I3,
+  },
+ ];
+
+  const imageLM = [
+  {
+    original: LMI,
+    thumbnail: LMI,
+  },
+
+   {
+    original: LMI2,
+    thumbnail: LMI2,
+  },
+
+   {
+    original: LMI3,
+    thumbnail: LMI3,
+  },
+ ];
+
+ const imageTV = [
+  {
+    original: TVI,
+    thumbnail: TVI,
+  },
+
+   {
+    original: TVI2,
+    thumbnail: TVI2,
+  },
+
+   {
+    original: TVI3,
+    thumbnail: TVI3,
+  },
+ ];
+
+ const imageFM = [
+  {
+    original: FMI,
+    thumbnail: FMI,
+  },
+
+   {
+    original: FMI2,
+    thumbnail: FMI2,
+  },
+
+   {
+    original: FMI3,
+    thumbnail: FMI3,
+  },
+ ];
+
+  const imageVE = [
+  {
+    original: VEI,
+    thumbnail: VEI,
+  },
+
+   {
+    original: VEI2,
+    thumbnail: VEI2,
+  },
+
+   {
+    original: VEI3,
+    thumbnail: VEI3,
+  },
+ ];
+
+  const imageCE = [
+  {
+    original: CEI,
+    thumbnail: CEI,
+  },
+
+   {
+    original: CEI2,
+    thumbnail: CEI2,
+  },
+
+   {
+    original: CEI3,
+    thumbnail: CEI3,
+  },
+ ];
 
 
 
@@ -112,60 +614,135 @@ function chooseImage () {
         return(<div className="img-gallery">
        <ImageGallery items={images} /></div>)
     } else if (selectedPopUp === "ford") {
-        return(<div><img src ={FordI}className="fordI" /> <img src ={FordI2}className="fordI2" /> <img src ={FordI3}className="fordI3" ></img></div>)
+        return(<div className="img-gallery">
+       <ImageGallery items={imageF} /></div>)
     } else if (selectedPopUp === "dodge") {
-        return(<img src={"dodge"}></img>)
+         return(<div className="img-gallery">
+       <ImageGallery items={imageD} /></div>)
+    } else if (selectedPopUp === "dodge2") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageD2} /></div>)
+    } else if (selectedPopUp === "ram") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageR} /></div>)
+    } else if (selectedPopUp === "jeepc") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageJC} /></div>)
+    } else if (selectedPopUp === "jeepr") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageJR} /></div>)
+    } else if (selectedPopUp === "jeepw") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageJW} /></div>)
+    } else if (selectedPopUp === "jeepl") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageJL} /></div>)
+    } else if (selectedPopUp === "jeeps") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageJS} /></div>)
+    } else if (selectedPopUp === "cart") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageTC} /></div>)
+     } else if (selectedPopUp === "carc") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageTC2} /></div>)
+     } else if (selectedPopUp === "carr") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageTRD} /></div>)
+     } else if (selectedPopUp === "carff") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageFI} /></div>)
+     } else if (selectedPopUp === "fordff") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageSE} /></div>)
+     } else if (selectedPopUp === "suvv") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageRV} /></div>)
+     } else if (selectedPopUp === "suvh") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageRV2} /></div>)
+     } else if (selectedPopUp === "suva") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageSO} /></div>)
+      } else if (selectedPopUp === "suvf") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageSF} /></div>)
+      } else if (selectedPopUp === "suvo") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageSF2} /></div>)
+      } else if (selectedPopUp === "vans") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageLM} /></div>)
+      } else if (selectedPopUp === "vanss") {
+         return(<div className="img-gallery">
+       <ImageGallery items={imageTV} /></div>)
+      } else if (selectedPopUp === "vanc") {
+        return(<div className="img-gallery">
+       <ImageGallery items={imageFM} /></div>)
+      } else if (selectedPopUp === "vanf") {
+        return(<div className="img-gallery">
+       <ImageGallery items={imageVE} /></div>)
+      } else if (selectedPopUp === "vanb") {
+        return(<div className="img-gallery">
+       <ImageGallery items={imageCE} /></div>)
+      } else if (selectedPopUp === "scf") {
+        return(<div className="img-gallery">
+       <ImageGallery items={imageCE} /></div>)
+
+
+
+
+
 }
 }
 
 
   const truckMenu = () => {
   return (<div><h1>Truck list - Ford F-150 Raptor</h1><img onClick={(e)=>handleShowPopUp(e)} id="ford" src ={Truck1}className="ford"/>
-
   <h1>Truck list - Toyota Tacoma</h1><img onClick={(e)=>handleShowPopUp(e)} id="toyota_truck" src ={Truck2}className="toyota_truck"/>
   <h1>Truck list -  Dodge Cummins 2500/3500</h1><img onClick={(e)=>handleShowPopUp(e)} id="dodge" src ={Truck3}className="dodge"/>
-  <h1>Truck list -  1st Gen Dodge Cummins</h1><img src ={Truck4}className="dodge2"/>
-  <h1>Truck list -  Ram 1500 EcoDiesel</h1><img src ={Truck5}className="ram"/></div>)
+  <h1>Truck list -  1st Gen Dodge Cummins</h1><img onClick={(e)=>handleShowPopUp(e)} id="dodge2" src ={Truck4}className="dodge2"/>
+  <h1>Truck list -  Ram 1500 EcoDiesel</h1><img onClick={(e)=>handleShowPopUp(e)} id="ram" src ={Truck5}className="ram"/></div>)
 
 }
 const jeepMenu = () => {
-  return (<div><h1>Jeep list - Jeep Compass 2018</h1><img src ={Jeep1}className="jeepc"/>
-  <h1>Jeep list - V8-Powered Jeep Wrangler Rubicon 392</h1><img src ={Jeep2}className="jeepr"/>
-  <h1>Jeep list - 2020 Jeep Wrangler Rubicon Unlimited EcoDiesel</h1><img src ={Jeep3}className="jeepw"/>
-  <h1>Jeep list - 2011 Jeep Liberty</h1><img src ={Jeep4}className="jeepl"/>
-  <h1>Jeep list - 2019 Jeep Wrangler Unlimited Sahara</h1><img src ={Jeep5}className="jeeps"/></div>)
+  return (<div><h1>Jeep list - Jeep Compass 2018</h1><img onClick={(e)=>handleShowPopUp(e)} id="jeepc" src ={Jeep1}className="jeepc"/>
+  <h1>Jeep list - V8-Powered Jeep Wrangler Rubicon 392</h1><img onClick={(e)=>handleShowPopUp(e)} id="jeepr" src ={Jeep2}className="jeepr"/>
+  <h1>Jeep list - 2020 Jeep Wrangler Rubicon Unlimited EcoDiesel</h1><img onClick={(e)=>handleShowPopUp(e)} id="jeepw" src ={Jeep3}className="jeepw"/>
+  <h1>Jeep list - 2011 Jeep Liberty</h1><img onClick={(e)=>handleShowPopUp(e)} id="jeepl" src ={Jeep4}className="jeepl"/>
+  <h1>Jeep list - 2019 Jeep Wrangler Unlimited Sahara</h1><img onClick={(e)=>handleShowPopUp(e)} id="jeeps" src ={Jeep5}className="jeeps"/></div>)
 }
 
 const carMenu = () => {
-  return (<div><h1>Sedan list - 2020 Toyota Camry Hybrid</h1><img src ={Car1}className="cart"/>
-  <h1>Sedan list - 1997 Toyota Corolla</h1><img src ={Car2}className="carc"/>
-  <h1>Sedan list - 2020 Toyota Camry TRD</h1><img src ={Car3}className="carr"/>
-  <h1>Sedan list - 2017 Ford Fusion Hybrid</h1><img src ={Car4}className="carff"/>
-  <h1>Sedan list - 2012 Ford Focus SE</h1><img src ={Car5}className="fordff"/></div>)
+  return (<div><h1>Sedan list - 2020 Toyota Camry Hybrid</h1><img onClick={(e)=>handleShowPopUp(e)} id="cart" src ={Car1}className="cart"/>
+  <h1>Sedan list - 1997 Toyota Corolla</h1><img onClick={(e)=>handleShowPopUp(e)} id="carc" src ={Car2}className="carc"/>
+  <h1>Sedan list - 2020 Toyota Camry TRD</h1><img onClick={(e)=>handleShowPopUp(e)} id="carr" src ={Car3}className="carr"/>
+  <h1>Sedan list - 2017 Ford Fusion Hybrid</h1><img onClick={(e)=>handleShowPopUp(e)} id="carff" src ={Car4}className="carff"/>
+  <h1>Sedan list - 2012 Ford Focus SE</h1><img onClick={(e)=>handleShowPopUp(e)} id="fordff" src ={Car5}className="fordff"/></div>)
 }
 
 const suvMenu = () => {
-  return (<div><h1>Suv list - 2020 Honda CRV</h1><img src ={Suv1}className="suvv"/>
-  <h1>Suv list - 2019 Honda CRV</h1><img src ={Suv2}className="Suvh"/>
-  <h1>Suv list -2020 Subaru Outback</h1><img src ={Suv3}className="suva"/>
-  <h1>Suv list - 2020 Subaru Forester</h1><img src ={Suv4}className="suvf"/>
-  <h1>Suv list - 2021 Subaru Accent</h1><img src ={Suv5}className="suvo"/></div>)
+  return (<div><h1>Suv list - 2020 Honda CRV</h1><img onClick={(e)=>handleShowPopUp(e)} id="suvv" src ={Suv1}className="suvv"/>
+  <h1>Suv list - 2019 Honda CRV</h1><img onClick={(e)=>handleShowPopUp(e)} id="suvh" src ={Suv2}className="suvh"/>
+  <h1>Suv list -2020 Subaru Outback</h1><img onClick={(e)=>handleShowPopUp(e)} id="suva" src ={Suv3}className="suva"/>
+  <h1>Suv list - 2020 Subaru Forester</h1><img onClick={(e)=>handleShowPopUp(e)} id="suvf" src ={Suv4}className="suvf"/>
+  <h1>Suv list - 2021 Subaru Accent</h1><img onClick={(e)=>handleShowPopUp(e)} id="suvo" src ={Suv5}className="suvo"/></div>)
 }
 
 const vanMenu = () => {
-  return (<div><h1>Van list - 2020 Lexus MiniVan</h1><img src ={Van1}className="vans"/>
-  <h1>Van list - 2006 Ford Transit Van</h1><img src ={Van2}className="vanss"/>
-  <h1>Van list - 2007 Ford MiniVan</h1><img src ={Van3}className="vanc"/>
-  <h1>Van list - 2020 Ford Van Eco</h1><img src ={Van4}className="vanf"/>
-  <h1>Van list - 2002 Chevrolet Express Van</h1><img src ={Van5}className="vanb"/></div>)
+  return (<div><h1>Van list - 2020 Lexus MiniVan</h1><img onClick={(e)=>handleShowPopUp(e)} id="vans" src ={Van1}className="vans"/>
+  <h1>Van list - 2006 Ford Transit Van</h1><img onClick={(e)=>handleShowPopUp(e)} id="vanss" src ={Van2}className="vanss"/>
+  <h1>Van list - 2007 Ford MiniVan</h1><img onClick={(e)=>handleShowPopUp(e)} id="vanc" src ={Van3}className="vanc"/>
+  <h1>Van list - 2020 Ford Van Eco</h1><img onClick={(e)=>handleShowPopUp(e)} id="vanf" src ={Van4}className="vanf"/>
+  <h1>Van list - 2002 Chevrolet Express Van</h1><img onClick={(e)=>handleShowPopUp(e)} id="vanb" src ={Van5}className="vanb"/></div>)
 }
 
 const scarMenu = () => {
-  return (<div><h1>Sports car list - 2021 Ferrari F8 Topless </h1><img src ={SC1}className="scf"/>
-  <h1>Sports car list - 2021 Ferrari J50</h1><img src ={SC2}className="scff"/>
-  <h1>Sports car list - 2020 Ferrari F8 Tributo</h1><img src ={SC3}className="sct"/>
-  <h1>Sports car list - 1999 Ferrari Porchse  </h1><img src ={SC4}className="scp"/>
-  <h1>Sports car list - 2019 Ferrari 250 GTO</h1><img src ={SC5}className="scc"/></div>)
+  return (<div><h1>Sports car list - 2021 Ferrari F8 Topless</h1><img onClick={(e)=>handleShowPopUp(e)} id="scf" src ={SC1}className="scf"/>
+  <h1>Sports car list - 2021 Ferrari J50</h1><img onClick={(e)=>handleShowPopUp(e)} id="scff" src ={SC2}className="scff"/>
+  <h1>Sports car list - 2020 Ferrari F8 Tributo</h1><img onClick={(e)=>handleShowPopUp(e)} id="sct" src ={SC3}className="sct"/>
+  <h1>Sports car list - 1999 Ferrari Porchse</h1><img onClick={(e)=>handleShowPopUp(e)} id="scp" src ={SC4}className="scp"/>
+  <h1>Sports car list - 2019 Ferrari 250 GTO</h1><img onClick={(e)=>handleShowPopUp(e)} id="scc" src ={SC5}className="scc"/></div>)
 }
 
 function handleOnClick (e) {
@@ -214,17 +791,17 @@ function handleOnClick (e) {
       <div className="car">
       {(showPopUp === true) ? popUp : <div />}
       <h1>List of cars please take a good look around!!</h1>
-      <button id="truck" onClick={(e) => handleOnClick(e)}>Trucks</button>
-      {(truckDropDown === true)? truckMenu() : <div /> }
-      <button id="jeep" onClick={(e) => handleOnClick(e)}>Jeeps</button>
+      <button className="custom-button" id="truck" onClick={(e) => handleOnClick(e)}>Trucks</button>
+      {(truckDropDown === true)? truckMenu() : <div/> }
+      <button className="custom-button" id="jeep" onClick={(e) => handleOnClick(e)}>Jeeps</button>
       {(jeepDropDown === true)? jeepMenu() : <div /> }
-      <button id="car" onClick={(e) => handleOnClick(e)}>Cars</button>
+      <button className="custom-button" id="car" onClick={(e) => handleOnClick(e)}>Cars</button>
       {(carDropDown === true)? carMenu() : <div /> }
-      <button id="suv" onClick={(e) => handleOnClick(e)}>Suv</button>
+      <button className="custom-button" id="suv" onClick={(e) => handleOnClick(e)}>Suv</button>
       {(suvDropDown === true)? suvMenu() : <div /> }
-      <button id="van" onClick={(e) => handleOnClick(e)}>Van</button>
+      <button className="custom-button" id="van" onClick={(e) => handleOnClick(e)}>Van</button>
       {(vanDropDown === true)? vanMenu() : <div /> }
-      <button id="scar" onClick={(e) => handleOnClick(e)}>Sport cars</button>
+      <button className="custom-button" id="scar" onClick={(e) => handleOnClick(e)}>Sport cars</button>
       {(scarDropDown === true)? scarMenu() : <div /> }
       </div>
 
--- core_frontend/src/components/information.js
+++ core_frontend/src/components/information.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Info from './info.jpg'
 import Loyalty from './pimg.jpg'
+import AD from './ad2.jpg'
 
 
 function Information() {
@@ -8,24 +9,37 @@ function Information() {
     <div className="container1">
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet"/>
     <div className="information">
-    <h1>Information page!</h1>
-
     <h2>Services we offer!</h2>
 
-    <h3> Loyalty programs </h3>
+
+
+    <div className="h1container">
+    <h1>Information page!</h1>
+
+
+
+    <a2 href="#" data-text="Loyalty program!">Loyalty program!</a2>
     <link href="https://fonts.googleapis.com/css2?family=PT+Serif&display=swap" rel="stylesheet"/>
-    <p2> Loyalty has its rewards. Current lessees can save up to $850 when you lease or buy another vehicale.</p2>
+    <p2> Loyalty has its rewards. Current lessees can save up to $850 when you lease or buy another vehicale and save you a whole lotta peace of mind.</p2>
 
     <img src={Loyalty} className="pimg"/>
+    <img src={AD} className="ad2"/>
 
      <p> At the end of your Toyota or Scion lease, you can save up to $850 when you lease or buy a new Toyota through Southeast Toyota Finance. It's our way of saying thank you. Plus, when you combine the loyalty savings with our low payments, you could possibly drive a new Toyota for less than you're paying now.
-LOYALTY CASH OFFER can be combined with cash back, special APR or lease programs and certain other offers. The offer is not transferable in any way. Offer entitles lease customer to $500 Loyalty Cash on the retail purchase or lease of any new Toyota vehicle through Southeast Toyota Finance and retailed and delivered within 30 days of lease maturity date. Loyalty cash must be used toward down payment/capitalized cost reduction and included in the buyer's order and/or contract. Offer valid only at participating Southeast Toyota Finance dealers in AL, FL, GA, NC or SC. Dealers must allow 6-8 weeks for processing. Incomplete or missing information will delay processing. Limit one per customer. $350 disposition fee waiver offer is applicable to current Southeast Toyota Finance lessees who turn in the leased vehicle and, within 30 days, finance or lease a new Toyota through Southeast Toyota Finance. $350 will be applied as a credit or issued as a rebate based on timing of the new finance or lease account and upon final resolution of prior lease account. New vehicle must be in the name of the original lessee or co-lessee.</p>
+LOYALTY CASH OFFER can be combined with cash back, special APR or lease programs and certain other offers. The offer is not transferable in any way. Offer entitles lease customer to $500 Loyalty Cash on the retail purchase or lease of any new Toyota vehicle through Southeast Toyota Finance and retailed and delivered within 30 days of lease maturity date. Loyalty cash must be used toward down payment/capitalized cost reduction and included in the buyer's
+order and/or contract. Offer valid only at participating Southeast Toyota Finance dealers in AL, FL, GA, NC or SC. Dealers must allow 6-8 weeks for processing. Incomplete or missing information will delay processing. Limit one per customer. $350 disposition fee waiver offer is applicable to current Southeast Toyota Finance lessees who turn in the leased vehicle and, within 30 days, finance or lease a new Toyota through Southeast Toyota Finance. $350 will be applied as a credit or issued as a rebate based on timing of the new finance or lease account and upon final resolution of prior lease account. New vehicle must be in the name of the original lessee or co-lessee.</p><link href="https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed&display=swap" rel="stylesheet"/>
+   </div>
 
+    <div className="container2">
+
+    <a3 href="#" data-text2="Auto services!">Auto services!</a3>
+    </div>
 
 
 
     </div>
     </div>
+
 
    );
  }
